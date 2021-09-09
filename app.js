d9f9800414eaee89ac809c5376b347341bf6a568
Daniel Florez <danielflorez111@hotmail.com>
2021-09-09 12:11:24 +0200

    Fixes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let fullName = prompt("Name: ");
+const fullName = prompt("Name: ");
 document.getElementById("fullName").innerHTML = fullName;
 
-let position = prompt("Position: ");
+const position = prompt("Position: ");
 document.getElementById("position").innerHTML = position;
 
-let email = prompt("Email: ");
+const email = prompt("Email: ");
 document.getElementById("email").innerHTML = email;
 document.getElementById("email").href = `mailto:${email}`;
 
-let phone = prompt("Phone: ");
+const phone = prompt("Phone: ");
 document.getElementById("phone").innerHTML = phone;
 document.getElementById("phone").href = `tel:${phone}`;
